3a2f0a518215c617b2770bf02a0e8e705df71c7c
Valerian <pvalera@gmail.com>
2024-08-24 13:58:37 +0300

    add web

--- sales_predictor/predictor/views.py
+++ sales_predictor/predictor/views.py
@@ -1,9 +1,22 @@
 # predictor/views.py
 import os
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+import pickle
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
+
+# Определение категориальных и числовых признаков
+categorical_column = ['StateHoliday', 'StoreType', 'Assortment', 'PromoInterval']
+num_column = ['Sales', 'Customers', 'Open', 'Promo', 'SchoolHoliday',
+                      'CompetitionDistance', 'CompetitionOpenSinceMonth',
+                      'Promo2', 'Promo2SinceWeek'
+                      ]
+
+date_column = ['Promo2SinceYear', 'CompetitionOpenSinceYear', "Date"]
 
 
 def index(request):
@@ -28,9 +41,15 @@ def predict_sales(request):
         # Обработка загруженного файла
         try:
             df = pd.read_csv(file_path)
-            results = process_data(df)
-            result_file_path = os.path.join(settings.MEDIA_ROOT, 'predictions.csv')
-            results.to_csv(result_file_path, index=False)
+            df = prepareData(df)
+            # Загрузите модель из файла
+
+            with open(settings.MEDIA_ROOT+'\\models\\model.pkl', 'rb') as file:
+                model = pickle.load(file)
+
+                results = model.predict(df)
+                result_file_path = os.path.join(settings.MEDIA_ROOT, 'predictions.csv')
+                results.to_csv(result_file_path, index=False)
 
             # Отправка файла с результатами пользователю
             with open(result_file_path, 'rb') as f:
@@ -42,8 +61,45 @@ def predict_sales(request):
 
     return HttpResponse('Invalid request method or missing file', status=400)
 
+def prepareData(df):
+    df_form = df.copy()
 
-def process_data(df):
-    # Пример обработки данных и создания результатов
-    df['Predictions'] = df['Sales'] * 1.1  # Простое увеличение значений для примера
-    return df
+    store_data = pd.read_csv(
+        settings.MEDIA_ROOT+'\\store\\store.csv',
+        encoding='iso-8859-1', na_values=[''], keep_default_na=False)
+    store_data = store_data.fillna("0")
+    df = pd.merge(df_form, store_data, on='Store')
+
+    df_form.replace({' - ': np.nan, '\\N': np.nan, 'NaN': np.nan}, inplace=True)
+    df_form = df_form.dropna()
+
+    # Факторизация категориальных столбцов
+    encoder = OneHotEncoder(sparse_output=False)
+    encoded_df_list = []
+
+    for name in categorical_column:
+        # Применение OneHotEncoder к целевому столбцу
+        encoded_columns = encoder.fit_transform(df_form[[name]])
+        # Преобразование закодированных данных в DataFrame
+        encoded_df = pd.DataFrame(encoded_columns, columns=encoder.get_feature_names_out([name]))
+        encoded_df_list.append(encoded_df)
+
+    # Объединение закодированных столбцов с исходным DataFrame (удалив при этом исходные столбцы)
+    df_form = df_form.drop(columns=categorical_column)
+    df_form = pd.concat([df_form] + encoded_df_list, axis=1)
+
+    df_form['Year'] = df_form['Date'].dt.year - 2000
+    df_form['Month'] = df_form['Date'].dt.month
+    df_form['Day'] = df_form['Date'].dt.day
+    del df_form['Date']
+    # Приведение всех числовых столбцов к типу int
+    for column in df_form.columns:
+        df_form[column] = pd.to_numeric(df_form[column], errors='coerce')
+
+    df_form = df_form.dropna()
+
+    df_form['CompetitionOpenSinceYear'] -= 2000
+    df_form.loc[df_form['Promo2SinceYear'] > 2000, 'Promo2SinceYear'] -= 2000
+
+    return df_form
+
